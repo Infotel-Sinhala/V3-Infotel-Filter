2b8a38aebb66bde2e31105249b1eab91f1abcbca
MRK-YT <81806065+MRK-YT@users.noreply.github.com>
2021-09-01 23:37:31 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -4,6 +4,7 @@
 
 from pyrogram import filters, Client
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
+from pyrogram.errors import UserNotParticipant
 from bot import Translation, LOGGER # pylint: disable=import-error
 from bot.database import Database # pylint: disable=import-error
 from bot import MT_UPDATE, MT_GROUP, MT_CHANNEL, MT_LINK
